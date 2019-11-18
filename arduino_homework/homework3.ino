88a520cc2841b8d2aa2b2b62bfcefd8f4ae3c6fc
CrazyFire <fdff87554@gmail.com>
2019-11-18 20:32:41 +0800

    finish homework 3 code

--- arduino_homework/homework3.ino
+++ arduino_homework/homework3.ino
@@ -1,101 +1,148 @@
-#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 0x20,0x2);	// lcd set
 
-LiquidCrystal_I2C lcd(0x27,16,2); 
+long duration, dis;
+int echoPin = 9;						// the pin of Ultrasonic echo
+int triPin = 10;						// the pin of Ultrasonic tri
+int soundPin = 5;						// the pin of buzzer
+int pred = 0, light_f = 0, sound_f = 0;
 
-long duration, distance;
-
-// trig is output
-// echo is input
-int trigPin= 10;
-int echoPin= 9;
-
-void ultrasonicOut(){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+void setup(){
+	lcd. init();						// LCD init
+	lcd. backlight();					// open backlight
+	for( int i = 2; i <= 4; i++ ){
+		pinMode(i, OUTPUT);				// LED output
+	}
+	pinMode(triPin, OUTPUT);			// Ultrasonic output
+	pinMode(echoPin, INPUT);			// Ultrasonic input
+	pinMode(soundPin, OUTPUT);			// buzzer output
+	Serial.begin(9600);					// the link of computer
 }
 
-void ultrasonicBack(){
-  duration = pulseIn(echoPin,HIGH);
+void soundOut( int d,bool same ){
+	if(!same){							// if the set of length is change, change the soundset
+		sound_f = 0;
+	}
+	if(d>=30){							// no sound, delay one second for break
+		delay(1000);
+		return;
+	}
+	if(d<10){
+		tone(5,1466,1000);				// keep ringing
+	}
+	else if(d<20){						// two second as a cycle, one second ring five time
+		if(sound_f==0){
+			for(int i=0;i<5;i++){
+				tone(5,1066+100*i,100);
+				delay(100);
+			}
+		}
+		else{
+			delay(1000);
+		}
+		sound_f=(sound_f+1)%3;
+	}
+	else if(d<30){						// five second a cycle, one second ring five time
+		if(sound_f==0){
+			for(int i=0;i<5;i++){
+				tone(5,1066+100*i,100);
+				delay(100);
+			}
+		}
+		else{
+			delay(1000);
+		}
+		sound_f=(sound_f+1)%6;
+	}
 }
 
-void count(){
-  distance = (duration / 2) / 29.1;
+void LCDOut( int d ){
+	// to avoid the delay of lcd.clear();
+	// clear the first line of LCD
+	lcd.setCursor(0, 0);
+	lcd.print("         ");
+	// output the different type for different length
+	lcd.setCursor(0, 0);
+	if( d <= 10 ){
+		lcd.print("Urgency");
+	}
+	else if( d <= 30 ){
+		lcd.print("Attention");
+	}
+	else{
+		lcd.print("Safely");
+	}
+	// clear second line of LCD
+	lcd.setCursor(0, 1);
+	lcd.print("       ");
+	// output the length ultrasonic get in second line;
+	lcd.setCursor(0, 1);				// set the output at second line
+	lcd.print(d);						// output length
 }
 
-void lightOn(int i) {
-  digitalWrite(i,  HIGH);
+void LEDOut( int d, bool same ){
+	if(!same){
+		light_f=0;
+	}
+	if(0<=d && d<10){
+		digitalWrite(2,HIGH);
+		digitalWrite(3,LOW);
+		digitalWrite(4,LOW);
+	}
+	else if(d>=10 && d<20){
+		digitalWrite(3,LOW);
+		digitalWrite(4,LOW);
+		if(light_f==0){
+			digitalWrite(2,HIGH);
+		}
+		else{
+			digitalWrite(2,LOW);
+		}
+		light_f=(light_f+1)%2;
+	}
+	else if(d>=20 && d<30){
+		digitalWrite(2,LOW);
+		digitalWrite(3,HIGH);
+		digitalWrite(4,LOW);
+	}
+	else{
+		digitalWrite(2,LOW);
+		digitalWrite(3,LOW);
+		digitalWrite(4,HIGH);
+	}
 }
 
-void lightOff() {
-  digitalWrite(2,  LOW);
-  digitalWrite(3,  LOW);
-  digitalWrite(4,  LOW);
+void ultrasonicWork(){					// set the frequency of ultrasonic
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(5);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+	duration = pulseIn(echoPin,HIGH);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600);
-  for(int i = 0;i <= 6;i++){
-    pinMode(i,  OUTPUT);
-  }
-  //LCD reset
-  lcd.init();
-  lcd.backlight();
+bool change_mode(int d, int pre){		// check out if the mode is the same as the one before
+	bool same=false;
+	if(d>0 && d<=10 && pre>0 && pre<=10){
+		return true;
+	}
+	if(d>10 && d<20 && pre>10 && pre<20){
+		return true;
+	}
+	if(d>20 && d<30 && pre>20 && pre<30){
+		return true;
+	}
+	if(d>30 && pre>30){
+		return true;
+	}
+	return false;
 }
 
 void loop(){
-  ultrasonicOut();
-  ultrasonicBack();
-  count();
-  delay(500);
+	ultrasonicWork();
+	dis = (duration/2)/29.1;			// count out the length in CM
+	LCDOut(dis);
+	LEDOut(dis);
+	soundOut(dis);
 }
-
-
-  if( distance < 10 ){
-    lightOff();
-    lcd.setCursor(0,0);
-    lcd.print("Urgency");
-    lcd.setCursor(0,1);
-    lcd.print(distance);
-    lightOn(2);
-    tone( 11, 1976 );
-  }
-  
-  if( distance >= 10 && distance < 20 ){
-    lightOff();
-    lcd.setCursor(0,0);
-    lcd.print("Attention");
-    lcd.setCursor(0,1);
-    lcd.print(distance);
-    tone( 11, 1976, 500 );
-    delay(500);
-    lightOn(2);
-    delay(1000);
-  }
-
-  if( distance >= 20 && distance < 30 ){
-    lightOff();
-    lcd.setCursor(0,0);
-    lcd.print("Attention");
-    lcd.setCursor(0,1);
-    lcd.print(distance);
-    lightOn(3);
-    tone( 11, 1976, 500 );
-    delay(4500);
-  }
-  
-  if( distance >= 30 ){
-    lightOff();
-    lcd.setCursor(0,0);
-    lcd.print("Safety");
-    lcd.setCursor(0,1);
-    lcd.print(distance);
-    lightOn(4);
-  }
-  delay(500);
